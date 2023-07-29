724b744cb2329372383f9df840c49b63ee4e064e
Tal Lovton <78549129+TalLovton@users.noreply.github.com>
2023-07-29 14:47:36 +0300

    Update main.py

--- StockAlerts/main.py
+++ StockAlerts/main.py
@@ -4,93 +4,85 @@ import time
 from plyer import notification
 from Stock import Stock
 
-
+#extracting the latest stock prices, compare to user input and notificate
 def stockAlarm():
-    tickers = menu()
+    tickersMap = menu()
+    tickersList = list(tickersMap.keys())
     while True:
         # Retrieve the last adjusted close price for each ticker
-        lastPrices = [yf.Ticker(ticker.tickerStr.upper()).history(period='1d')["Close"].iloc[-1] for ticker in tickers]
-        for i in range(len(tickers)):
+        lastPrices = [yf.Ticker(ticker).history(period='1d')["Close"].iloc[-1] for ticker in tickersList]
+        for i in range(len(tickersList)):
             formatPrice = format(lastPrices[i], ".2f")
-            symbol = tickers[i].tickerStr
+            symbol = tickersList[i]
             print(symbol + ": " + formatPrice + "\n----------")
+
             # Check if any prices exceed the upper or lower limits
-            if lastPrices[i] <= tickers[i].lowerLimit:
-                # Send a notification and play a sound for prices exceeding the upper limit
-                notification.notify(
-                    app_name="Stock",
-                    title=f"Price Alert for {tickers[i].tickerStr}",
-                    message=f"{tickers[i].tickerStr} has reached your entry price!\n price of {formatPrice}! Buy?",
-                    timeout=5,
-                    toast=False,
-                    app_icon = "icons/buy.ico"
-
-                )
-            elif lastPrices[i] > tickers[i].upperLimit:
-                # Send a notification and play a sound for prices below the lower limit
-                notification.notify(
-                    app_name="Stock",
-                    title=f"Price Alert for {tickers[i].tickerStr}",
-                    message=f"{tickers[i].tickerStr} has reached your TP price!\n price of {formatPrice}! Sell?",
-                    timeout=5,
-                    toast=False,
-                    app_icon = "icons/buy.ico"
-
-                )
+            tmpEntryPrice = tickersMap.get(symbol).getEntryPrice()
+            rangeValue = tmpEntryPrice * 1.5 / 100
+            if tmpEntryPrice - rangeValue <= lastPrices[i] <= tmpEntryPrice + rangeValue:
+                alert(symbol,formatPrice,"reached your entry price")
+            
+            
+            elif lastPrices[i] >= tickersMap.get(symbol).getProfitPrice():
+                alert(symbol,formatPrice,"reached your take profit price")
+               
 
         time.sleep(10)
 
 
 def menu():
-    stockList = []
+    # creating the portfolio stocks list to be alerted
+    stockList = {}
     while (True):
-        tickerStr = input("Enter your stock tiker: ")
-        if isContain(stockList, tickerStr):
+        tickerStr = input("Enter your stock tiker: ").strip().upper()
+        if tickerStr in stockList:
+            changeVals(stockList,tickerStr)
             break
-        profitPrice = input("Enter your take profit price: ")
-        entryPrice = input("Enter your entry price: ")
+        entryPrice = input("Enter your entry price: ").strip()
+        profitPrice = input("Enter your take profit price: ").strip()
         # validation checks
         if checkValid(entryPrice, profitPrice):
-            newStock = Stock(tickerStr, entryPrice, profitPrice)
-            stockList.append(newStock)
-        else:
-            print("problem with your input, try again.")
-            continue
-        res = input("Another stock? y/n: ")
-        if res != 'y' and res != 'Y':
+            newStock = Stock(tickerStr, float(entryPrice), float(profitPrice))
+            stockList[tickerStr] = newStock
+        res = input("Another stock? y/n: ").strip().lower()
+        if res != 'y':
             break
     return stockList
 
-
+#chackin validations of user inputs
 def checkValid(entryPrice, profitPrice):
-    try:
-        float(profitPrice)
-        float(entryPrice)
-        return True
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-        return False
-
-
-def isContain(stocksList, ticker):
-    tickerSet = {stock.tickerStr for stock in stocksList}
-    if ticker in tickerSet or ticker.upper() in tickerSet:
-        flag = changeVals(stocksList, ticker)
-        return flag
-    return False
-
-
+    while True:
+        try:
+            float(profitPrice)
+            float(entryPrice)
+            return True
+        except ValueError:
+            print("Invalid input!. Please enter a valid number.")
+            
+#editing the values of existing stock at portfolio
 def changeVals(stocksList, ticker):
-    let = input("The stock is at your traiding list!\nwould you like to edit prices? y/n: ")
-    if (let != 'y' and let != 'Y'):
-        return False
-    newProfit = input("set your new profit price: ")
-    newStop = input("set your new stop price: ")
-    for stock in stocksList:
-        if stock.tickerStr == ticker:
-            stock.setLimitPrice(newProfit)
-            stock.setStopPrice(newStop)
-    return True
+    let = input("The stock is at your traiding list!\nwould you like to edit prices? y/n: ").strip().lower()
+    if (let != 'y'):
+        return
+    entryPrice = float(input("set your new entry price: ").strip())
+    profitPrice = float(input("set your new take profit price: ").strip())
+    if checkValid(entryPrice, profitPrice):
+        stocksList.get(ticker).setEntryPrice(entryPrice)
+        stocksList.get(ticker).setProfitPrice(profitPrice)
+        print("Changes are saved!")
+        
+# Send a notification and play a sound for prices reaching the entry price
+def alert(symbol,price,msg):
+    notification.notify(
+                    app_name="StockAlert",
+                    title=f"Price Alert for {symbol}",
+                    message=f"{symbol} {msg} !\nprice of {price}!!",
+                    timeout=5,
+                    toast=False,
+                    app_icon = "icons/buy.ico"
+
+                )
+
 
 
 if __name__ == "__main__":
